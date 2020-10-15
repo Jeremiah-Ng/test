54e1988716d7311df3fc266f92df02f217de4cb3
Jeremiah-Ng <72901442+Jeremiah-Ng@users.noreply.github.com>
2020-10-14 19:52:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function change(){
+    document.getElementById("s1").style.color="blue";
+    document.getElementById("hello").style.color="red";
+    window.alert("Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello")
+}
